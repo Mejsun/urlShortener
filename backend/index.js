8008008f8d5ba34b73bf5a9bd528723f391146b2
Mejsun <mejsunalghoul@gmail.com>
2022-02-19 21:13:13 +0000

    connect database

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-console.log('hello')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const connectDB = require('./config/db')
+connectDB()
+
+const defaultPort = process.env.BASE || 5000
+
+app.listen(()=> {
+    console.log(`Server is running on ${defaultPort}`)
+})
